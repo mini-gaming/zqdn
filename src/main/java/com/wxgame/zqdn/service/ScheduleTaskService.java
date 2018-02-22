de37b3eb9c779c9d2b4939240edfdae1bf25888f
fozhang <fozhang@ebay.com>
2018-02-22 21:17:46 +0800

    dev

--- src/main/java/com/wxgame/zqdn/service/ScheduleTaskService.java
+++ src/main/java/com/wxgame/zqdn/service/ScheduleTaskService.java
@@ -1,6 +1,7 @@
 package com.wxgame.zqdn.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,7 +10,7 @@ public class ScheduleTaskService {
 	@Autowired
 	private GameInfoService gameInfoService;
 	
-	//@Scheduled(cron = "0 0 2 * * ?")
+	@Scheduled(cron = "0 0 2 * * ?")
 	public void updateIdioms(){
 		
 		gameInfoService.updateIdioms(0);
